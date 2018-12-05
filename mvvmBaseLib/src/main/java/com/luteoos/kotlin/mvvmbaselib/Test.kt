58347772966f82a3e0767241f55deb3b68280b8a
Luteoos <lmati97@gmail.com>
2018-12-05 18:11:05 +0100

    random things to try if works on jitpack

---
+++ mvvmBaseLib/src/main/java/com/luteoos/kotlin/mvvmbaselib/Test.kt
@@ -0,0 +1,19 @@
+package com.luteoos.kotlin.mvvmbaselib
+
+import android.annotation.SuppressLint
+import io.realm.Realm
+
+@SuppressLint("ValidFragment")
+private class Test() {
+    private val vm = object : BaseViewModel(Realm.getDefaultInstance()){}
+    private val a = object : BaseActivityMVVM<BaseViewModel>(){
+        override fun getLayoutID(): Int = 0
+    }
+    private var b = object : BaseFragmentMVVM<BaseViewModel>(){
+        override fun getLayoutID(): Int = 0
+    }
+    private var c = object : BaseFragmentMVVMWithoutVM(){
+        override fun getLayoutID(): Int = 0
+    }
+
+}
