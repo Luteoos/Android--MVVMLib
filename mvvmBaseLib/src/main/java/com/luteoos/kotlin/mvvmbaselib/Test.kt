139a3dfb97d74c2626b0034c5acb45b0377b6203
Luteoos <lmati97@gmail.com>
2018-12-19 19:30:55 +0100

    no message

--- mvvmBaseLib/src/main/java/com/luteoos/kotlin/mvvmbaselib/Test.kt
+++ mvvmBaseLib/src/main/java/com/luteoos/kotlin/mvvmbaselib/Test.kt
@@ -7,12 +7,20 @@ import io.realm.Realm
 private class Test() {
     private val vm = object : BaseViewModel(){}
     private val a = object : BaseActivityMVVM<BaseViewModel>(){
+        override fun onVMMessage(msg: String?) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
         override fun getLayoutID(): Int {
             viewModel = vm
             return 0
         }
     }
     private var b = object : BaseFragmentMVVM<BaseViewModel>(){
+        override fun onVMMessage(msg: String?) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
         override fun getLayoutID(): Int {
             viewModel = vm
             return 0
