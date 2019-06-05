2695d0f2a0afa37beb77c0d272c707e5d1cf1436
Luteoos <lmati97@gmail.com>
2019-06-05 16:47:35 +0200

    added possibility to get vm in fragment from activity scope

--- mvvmBaseLib/src/main/java/com/luteoos/kotlin/mvvmbaselib/BaseFragmentMVVM.kt
+++ mvvmBaseLib/src/main/java/com/luteoos/kotlin/mvvmbaselib/BaseFragmentMVVM.kt
@@ -4,6 +4,7 @@ import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentActivity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -40,6 +41,10 @@ abstract class BaseFragmentMVVM<T: BaseViewModel> : BaseFragmentMVVMWithoutVM(){
 
     companion object {
         inline fun <reified T : BaseViewModel?> getViewModel(fragment: Fragment): T {
+            return ViewModelProviders.of(fragment).get(T::class.java)
+        }
+
+        inline fun <reified T : BaseViewModel?> getViewModel(fragment: FragmentActivity): T {
             return ViewModelProviders.of(fragment).get(T::class.java)
         }
     }
--- mvvmBaseLib/src/main/java/com/luteoos/kotlin/mvvmbaselib/Test.kt
+++ mvvmBaseLib/src/main/java/com/luteoos/kotlin/mvvmbaselib/Test.kt
@@ -8,6 +8,14 @@ private class Test() {
     object : BaseViewModel(){}
     private val a = object : BaseActivityMVVM<BaseViewModel>(){
 
+        private var b = object : BaseFragmentMVVM<BaseViewModel>() {
+
+            override fun getLayoutID(): Int {
+                viewModel = getViewModel(activity!!)
+                return 0
+            }
+        }
+
         override fun getLayoutID(): Int {
             viewModel = getViewModel(this)
             return 0
